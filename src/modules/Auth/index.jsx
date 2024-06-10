6fb96ce26e702cb888e1459af34036c698459064
pytuna <zikzikzikzik16@gmail.com>
2024-06-10 15:31:26 +0700

    first commit

---
+++ src/modules/Auth/index.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+function AuthModule() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />}></Route>
+    </Routes>
+  );
+}
+
+export default AuthModule;
